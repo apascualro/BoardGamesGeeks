a99f28f3ec8cb02e8cba3317facc703fcfe014a5
apascualro <apascualro@fp.uoc.edu>
2020-10-25 21:39:39 +0100

    port

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 /*=============================================
 El process es un objeto global que corre en todo el entorno de desarrollo de nodeJS
 =============================================*/
-const PORT = process.env.PORT || 4000;	
+const PORT = process.env.PORT || 8080;	
