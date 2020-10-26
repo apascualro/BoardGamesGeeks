e222294621f1e354edafca74445b55912cd2091d
apascualro <apascualro@fp.uoc.edu>
2020-10-26 21:09:40 +0100

    proxy fuera

--- src/app/services/juego.service.ts
+++ src/app/services/juego.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { HttpHeaders } from '@angular/common/http';
 
 //mismo port que en node.js
 const baseUrl = 'http://localhost:8080/';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    Authorization: 'my-auth-token'
+  })
+};
 
 
 @Injectable({
@@ -14,7 +22,7 @@ export class JuegoService {
 	constructor(private http: HttpClient) { }
 
 	getAll(): Observable<any> {
-		return this.http.get('api/juegos');
+		return this.http.get('http://localhost:8080/api/juegos');
 	}
 
 	get(id): Observable<any> {
