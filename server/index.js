c4f170dc0798b309a785a1c7e7ba411a63c71320
apascualro <apascualro@fp.uoc.edu>
2020-10-26 20:37:39 +0100

    proxy

--- server/index.js
+++ server/index.js
@@ -14,13 +14,13 @@ const app = express();
 // app.use(cors(corsOptions));
 app.use(cors());
 // Configurar cabeceras y cors
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+//     next();
+// });
 
 //sync database
 const db = require("./models");
@@ -33,11 +33,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // simple route HEROKU
-app.use(express.static('./dist/board-games-app'));
+// app.use(express.static('./dist/board-games-app'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/board-games-app/'}),
-);
+// app.get('/*', (req, res) =>
+//     res.sendFile('index.html', {root: 'dist/board-games-app/'}),
+// );
 // app.get("/", (req, res) => {
 //   res.json({ message: "Welcome to bezkoder application." });
 // });
