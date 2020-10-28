b514e15e4fa6008f5f056f17209cd29a5b11f490
apascualro <apascualro@fp.uoc.edu>
2020-10-28 07:43:45 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // simple route HEROKU
- // app.use(express.static('./dist/board-games-app'));
+ app.use(express.static('./dist/board-games-app'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/board-games-app/'}),
